0de2586723c62b896b12302c4b8eea355ec1ce06
shoxa <sllx.ok@gmail.com>
2024-02-17 17:43:22 +0500

    refactor: remove useless files

--- src/generateUserId.ts
+++
@@ -1,6 +0,0 @@
-let id = 0;
-
-export const generageUserId = () => {
-  id += 1;
-  return id;
-};
--- src/userDb.ts
+++
@@ -1,7 +0,0 @@
-import { UserDataClientType } from './type';
-
-type UserDbType = {
-  [x: string]: UserDataClientType;
-};
-
-export const userDb: UserDbType = {};
