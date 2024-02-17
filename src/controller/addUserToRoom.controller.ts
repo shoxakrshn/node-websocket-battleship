7bb9d4ebbbba399693816aaa95ec56dd0b614b8f
shoxa <sllx.ok@gmail.com>
2024-02-17 17:44:30 +0500

    feat: feat Add_User_To_Room controller

---
+++ src/controller/addUserToRoom.controller.ts
@@ -0,0 +1,27 @@
+import { Game } from '../model/Game.model';
+import { gameDb, roomDb, socketDb, userDb } from '../store/store';
+import { AddUserToRoomDataClinet } from '../types/dataTypes';
+import { eRequestType } from '../utils/constants';
+import { generateResponseDto } from '../utils/generateResponseDto.ts';
+import { broadcastRoom } from './updateRoom.controller';
+
+export const addUserToRoomController = (id: number, data: string) => {
+  const { indexRoom }: AddUserToRoomDataClinet = JSON.parse(data);
+  const room = roomDb.get(indexRoom);
+  const { index, name } = userDb.get(id);
+  room.addUser({ index, name });
+
+  const newGame = new Game();
+  room.roomUsers.forEach(({ index }) => newGame.addPlayer(index));
+  gameDb.set(newGame.gameId, newGame);
+
+  room.roomUsers.forEach(({ index }) => {
+    const updateGameJson = JSON.stringify({ idGame: newGame.gameId, idPlayer: index });
+    const updateGameAnswer = generateResponseDto(eRequestType.CreateGame, updateGameJson);
+    const socket = socketDb[index];
+    socket.send(updateGameAnswer);
+  });
+
+  roomDb.delete(indexRoom);
+  broadcastRoom();
+};
